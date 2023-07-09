9fed909491780d33f5fd1ceea282d773deb4f7e9
BosEriko <bos.eriko@gmail.com>
2023-07-09 20:28:48 +0800

    move dimension of video to css

--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -1,7 +1,7 @@
 export default function VideoContainer() {
   return (
     <div className="bg-gray-200 w-[1000px] aspect-video rounded-xl flex shadow-xl overflow-hidden">
-      <iframe className="block w-full h-full" width="560" height="315" src="https://www.youtube.com/embed/HPf6TU0AXp8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/HPf6TU0AXp8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   )
 }
