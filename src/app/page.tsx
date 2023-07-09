d430f1840e5747d6394a88130ada9bd36115ebd6
BosEriko <bos.eriko@gmail.com>
2023-07-09 19:40:58 +0800

    add background pattern as a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Pattern from '../components/Footer';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <div className="relative">
-        <div className="flex absolute inset-0 w-full h-full z-[-1]">
-          <div className="bg-white flex-1"></div>
-          <div className="diagonal-slash w-40"></div>
-          <div className="bg-[#f8efe6] flex-1"></div>
-        </div>
+        <Pattern />
         <div>
           <div className="container mx-auto">this is a test</div>
         </div>
---
+++ src/components/Pattern.tsx
@@ -0,0 +1,9 @@
+export default function Pattern() {
+  return (
+    <div className="flex absolute inset-0 w-full h-full z-[-1]">
+      <div className="bg-white flex-1"></div>
+      <div className="diagonal-slash w-40"></div>
+      <div className="bg-[#f8efe6] flex-1"></div>
+    </div>
+  )
+}
