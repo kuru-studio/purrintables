ed18ada67980616785a46209bd82564462ec2572
BosEriko <bos.eriko@gmail.com>
2023-07-09 19:47:07 +0800

    make the background slash more diagonal

--- src/components/Pattern.tsx
+++ src/components/Pattern.tsx
@@ -3,7 +3,7 @@ export default function Pattern({ children }: any) {
     <div className="relative">
       <div className="flex absolute inset-0 w-full h-full z-[-1]">
         <div className="bg-white flex-1"></div>
-        <div className="diagonal-slash w-40"></div>
+        <div className="diagonal-slash w-96"></div>
         <div className="bg-[#f8efe6] flex-1"></div>
       </div>
       <div>
