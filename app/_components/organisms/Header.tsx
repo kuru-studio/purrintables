2d4415b5eed3c4b16ba3554b86acdfc5b60e3878
BosEriko <bos.eriko@gmail.com>
2023-11-01 13:34:13 +0800

    Feature: Add a search bar

--- app/_components/organisms/Header.tsx
+++ app/_components/organisms/Header.tsx
@@ -1,19 +1,21 @@
-import Logo from '../../_assets/logo.png'
-import Image from 'next/image'
-
+import Atom from "@atom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Input } from "antd";
 
 const Header = () => {
   return (
     <header className="bg-[#b0abcb]">
       <nav className="text-white">
-        <div className="container mx-auto flex p-2 items-center">
-          <div className="mr-auto">
-            <Image src={Logo} width={150} alt='Purrintables'/>
-          </div>
+        <div className="container mx-auto flex p-3 items-center justify-between">
+          <div><Atom.Logo width={150} /></div>
           <div>
-            <FontAwesomeIcon icon={faBars} className="w-5 h-5" />
+            <Input
+              className="!rounded-none"
+              size="large"
+              placeholder="Search"
+              prefix={<FontAwesomeIcon icon={faMagnifyingGlass} className="w-5 h-5 mr-3 text-gray-400" />}
+            />
           </div>
         </div>
       </nav>
