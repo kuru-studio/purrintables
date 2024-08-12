ca1fb021ca88606c20b689076d448d69dcec3bc5
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-08-12 11:59:28 +0800

    Hotfix: Next 14 and remove deprec warnings (#9)
    # Story Title
    Hotfix: Next 14 and remove deprec warnings
    
    ## Changes Made
    
    - remove npm package-lock.json to resolve yarn install warning
    - install NextJS 14.2.5
    - remove next/head and use next/metadata to resolve nextjs deprecate warning
    - update ant design Dropdown to use menu prop instead of deprecated overlay prop 
    
    ## How does the solution address the problem
    This PR will upgrade the nextjs version to the latest (14.2.5) and remove deprecation warnings

--- app/_components/organisms/Navigation.tsx
+++ app/_components/organisms/Navigation.tsx
@@ -1,9 +1,13 @@
 "use client";
-import { useState } from 'react';
-import { Dropdown, Menu } from 'antd';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import { Dropdown, Menu, MenuProps } from "antd";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faCartShopping,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import Molecule from "@molecule";
 import Atom from "@atom";
 
@@ -16,27 +20,27 @@ const Navigation: React.FC<NavigationProps> = ({ type }) => {
   const [isCartDrawerOpen, setIsCartDrawerOpen] = useState(false);
   const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
 
-  const shopItems = [
-    'Shop All',
-    'Prints',
-    'Books',
-    'Stickers',
-    'Keychains',
-    'Standee',
-    'Tumbler / Mugs',
-    'Tshirts / Hoodies',
-    'Paocat Merch',
+  const shopItems: MenuProps["items"] = [
+    "Shop All",
+    "Prints",
+    "Books",
+    "Stickers",
+    "Keychains",
+    "Standee",
+    "Tumbler / Mugs",
+    "Tshirts / Hoodies",
+    "Paocat Merch",
   ].map((label, index) => ({
     key: index + 1,
     label,
   }));
 
-  const infoItems = [
-    'About Purrintables',
-    'News & FAQ',
-    'Shopee',
-    'Convention Schedule',
-    'Contact Us',
+  const infoItems: MenuProps["items"] = [
+    "About Purrintables",
+    "News & FAQ",
+    "Shopee",
+    "Convention Schedule",
+    "Contact Us",
   ].map((label, index) => ({
     key: index + 1,
     label,
@@ -46,48 +50,63 @@ const Navigation: React.FC<NavigationProps> = ({ type }) => {
     return (
       <div className="md:flex items-center text-white gap-5">
         <div>
-          <Dropdown overlay={<Menu items={shopItems} />}>
-            <a onClick={(e) => e.preventDefault()}>
-              Shop
-            </a>
+          <Dropdown menu={{ items: shopItems }}>
+            <a onClick={(e) => e.preventDefault()}>Shop</a>
           </Dropdown>
         </div>
         <div>
-          <Link href="/">
-            Home
-          </Link>
+          <Link href="/">Home</Link>
         </div>
         <div>
-          <Dropdown overlay={<Menu items={infoItems} />}>
-            <a onClick={(e) => e.preventDefault()}>
-              Info
-            </a>
+          <Dropdown menu={{ items: infoItems }}>
+            <a onClick={(e) => e.preventDefault()}>Info</a>
           </Dropdown>
         </div>
       </div>
     );
-  }
+  };
 
   const UserNavigation = () => {
     return (
       <div className="flex-1 flex justify-end gap-5">
         <Atom.Visibility state={isAuthenticateModalOpen}>
-          <Molecule.Authenticate isOpen={isAuthenticateModalOpen} setIsOpen={setIsAuthenticateModalOpen} />
+          <Molecule.Authenticate
+            isOpen={isAuthenticateModalOpen}
+            setIsOpen={setIsAuthenticateModalOpen}
+          />
         </Atom.Visibility>
         <Atom.Visibility state={isCartDrawerOpen}>
-          <Molecule.Cart isOpen={isCartDrawerOpen} setIsOpen={setIsCartDrawerOpen} />
+          <Molecule.Cart
+            isOpen={isCartDrawerOpen}
+            setIsOpen={setIsCartDrawerOpen}
+          />
         </Atom.Visibility>
         <Atom.Visibility state={isSearchModalOpen}>
-          <Molecule.Search isOpen={isSearchModalOpen} setIsOpen={setIsSearchModalOpen} />
+          <Molecule.Search
+            isOpen={isSearchModalOpen}
+            setIsOpen={setIsSearchModalOpen}
+          />
         </Atom.Visibility>
-        <FontAwesomeIcon icon={faUser} className="w-5 h-5 text-white cursor-pointer" onClick={() => setIsAuthenticateModalOpen(true)} />
-        <FontAwesomeIcon icon={faCartShopping} className="w-5 h-5 text-white cursor-pointer" onClick={() => setIsCartDrawerOpen(true)} />
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="w-5 h-5 text-white cursor-pointer" onClick={() => setIsSearchModalOpen(true)} />
+        <FontAwesomeIcon
+          icon={faUser}
+          className="w-5 h-5 text-white cursor-pointer"
+          onClick={() => setIsAuthenticateModalOpen(true)}
+        />
+        <FontAwesomeIcon
+          icon={faCartShopping}
+          className="w-5 h-5 text-white cursor-pointer"
+          onClick={() => setIsCartDrawerOpen(true)}
+        />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="w-5 h-5 text-white cursor-pointer"
+          onClick={() => setIsSearchModalOpen(true)}
+        />
       </div>
     );
-  }
+  };
 
-  switch(type) {
+  switch (type) {
     case "user":
       return <UserNavigation />;
     case "main":
@@ -95,6 +114,6 @@ const Navigation: React.FC<NavigationProps> = ({ type }) => {
     default:
       return <div>Unknown Navigation</div>;
   }
-}
+};
 
 export default Navigation;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,26 @@
-import './_styles/globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./_styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import Organism from "@organism";
 import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Purrintables',
-  description: 'This is the website of Purrintables',
-}
+  title: "Purrintables",
+  description: "This is the website of Purrintables",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
       <Organism.Wrapper>
         <body className={inter.className}>{children}</body>
       </Organism.Wrapper>
     </html>
-  )
+  );
 }
