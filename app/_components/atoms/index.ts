8d30712f5ed6e6ca728aef5c49a55551dfcdb59e
Edward Layao <edward.layao.dev@proton.me>
2024-08-14 09:56:43 +0800

    create Breadcrumb atom

--- app/_components/atoms/index.ts
+++ app/_components/atoms/index.ts
@@ -1,9 +1,11 @@
+import Breadcrumb from "./Breadcrumb";
 import Logo from "./Logo";
 import Visibility from "./Visibility";
 
 const Atom = {
   Logo,
   Visibility,
-}
+  Breadcrumb,
+};
 
-export default Atom
+export default Atom;
