e728bbe0e1e5ef596e9dcc6062cde961b6ae0199
Edward Layao <edward.layao.dev@proton.me>
2024-08-15 13:46:33 +0800

    upon dispatch add product to shopCart

--- app/_components/organisms/Shop.tsx
+++ app/_components/organisms/Shop.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Product } from "@/app/_types/product";
 import Molecule from "../molecules";
+import { useAtom } from "jotai";
+import { shopCartAtom } from "@/app/_contexts/shopCart";
+import { useEffect } from "react";
 
 interface Props {
   title: string;
@@ -9,9 +12,12 @@ interface Props {
 }
 
 export default function Shop({ title, productArray, isAddtoCart = false }: Props) {
+  const [_, setShopCart] = useAtom(shopCartAtom);
+  // updates shopCart upon dispatch
   function onAddToCart(product: Product) {
-    console.log(product);
+    setShopCart((prevValue) => [...prevValue, product]);
   }
+
   return (
     <div className="container mx-auto text-2xl py-10 px-5">
       <h4 className="font-bold text-center mb-5">{title}</h4>
