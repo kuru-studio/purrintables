f2ccb0b79fe3bbad72c8c4411d1c5ffc88e0c696
Edward Layao <edward.layao.dev@proton.me>
2024-08-21 10:45:30 +0800

    add JotaiProvider to clear instance warning

--- app/_components/organisms/Wrapper.tsx
+++ app/_components/organisms/Wrapper.tsx
@@ -2,14 +2,17 @@
 import StyledComponentsRegistry from "../../_utilities/antd-registry";
 import ConfigProvider from "antd/es/config-provider";
 import NotificationProvider from "@/app/_contexts/notification";
+import { Provider as JotaiProvider } from "jotai";
 
 const Wrapper = ({ children }: any) => {
   return (
-    <StyledComponentsRegistry>
-      <ConfigProvider>
-        <NotificationProvider>{children}</NotificationProvider>
-      </ConfigProvider>
-    </StyledComponentsRegistry>
+    <JotaiProvider>
+      <StyledComponentsRegistry>
+        <ConfigProvider>
+          <NotificationProvider>{children}</NotificationProvider>
+        </ConfigProvider>
+      </StyledComponentsRegistry>
+    </JotaiProvider>
   );
 };
 
